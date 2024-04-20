4af5ed0737772b8e420b883dd2482d7bce943aa4
indigoparadox <indigo.repo@interfinitydynamics.info>
2024-04-20 14:14:21 -0400

    Rough docs.

---
+++ README.md
@@ -0,0 +1,16 @@
+# floppcgi
+
+A tiny menu system for pushing floppy images to a Gotek floppy emulator via USB OTG.
+
+Designed for the Raspberry Pi Zero and [FlashFloppy Gotek firmware](https://github.com/keirf/flashfloppy).
+
+This project is written in C for maximum efficiency (and also because we just kinda wanted to?), as the Raspberry Pi Zero is not that powerful, but its USB-OTG functionality is essential for this purpose.
+
+More information may be found at [https://indigoparadox.zone/projects/gotek.html#Floppy-Server](https://indigoparadox.zone/projects/gotek.html#Floppy-Server).
+
+## Installation
+
+To install, clone this repo to (/opt/floppcgi is recommended) and run setup.sh.
+
+You MUST cd into the directory before running setup.sh. Do not try to run it from one level up!
+
---
+++ setup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+BOOT_CONFIG_PATH=/boot/firmware/config.txt
+NGINX_FCGI_CONFIG_PATH=/etc/nginx/fastcgi.conf
+SCRIPT_DIR="`dirname "$0"`"
+
+# Install required packages.
+apt install -y nginx libfcgi-dev mtools git || exit 1
+
+if [ ! -x "$SCRIPT_DIR/floppcgi" ]; then
+	echo "compiling floppcgi..."
+	make floppcgi || exit 1
+fi
+
+# Add dwc2 overlay for USB Gadget support.
+if ! grep -q "^dtoverlay=dwc2$" "$BOOT_CONFIG_PATH"; then
+	echo "adding dtoverlay=dwc2 to $BOOT_CONFIG_PATH..."
+	echo "dtoverlay=dwc2" >> "$BOOT_CONFIG_PATH"
+fi
+
+# Add dwc2 module for USB Gadget support.
+if ! grep -q "^dwc2$" "/etc/modules"; then
+	echo "adding dwc2 to /etc/modules..."
+	echo "dwc2" >> /etc/modules
+fi
+
+# Add config variables to FastCGI environment.
+if ! grep -q "^fastcgi_param\s*FLOPPIES_ROOT" $NGINX_FCGI_CONFIG_PATH; then
+	echo "adding FLOPPIES_ROOT to $NGINX_FCGI_CONFIG_PATH..."
+	echo "fastcgi_param  FLOPPIES_ROOT  /srv/Floppies;" \
+		>> "$NGINX_FCGI_CONFIG_PATH"
+fi
+
+if ! grep -q "^fastcgi_param\s*FLOPPIES_CONTAINER" $NGINX_FCGI_CONFIG_PATH
+then
+	echo "adding FLOPPIES_CONTAINER to $NGINX_FCGI_CONFIG_PATH..."
+	echo "fastcgi_param  FLOPPIES_CONTAINER  /tmp/floppyc.img;" \
+		>> "$NGINX_FCGI_CONFIG_PATH"
+fi
+
+if ! grep -q "^fastcgi_param\s*FLOPPIES_ASSETS" $NGINX_FCGI_CONFIG_PATH
+then
+	echo "adding FLOPPIES_ASSETS to $NGINX_FCGI_CONFIG_PATH..."
+	echo "fastcgi_param  FLOPPIES_ASSETS /opt/floppcgi;" \
+		>> "$NGINX_FCGI_CONFIG_PATH"
+fi
+
+# Copy the nginx service to /etc/systemd/system.
+if [ ! -f /etc/systemd/system/floppcgi.service ]; then
+	echo "installing systemd service..."
+	cp floppcgi.service /etc/systemd/system || exit 1
+	sed -i "s|/opt/floppcgi|$SCRIPT_DIR|g" \
+		/etc/systemd/system/floppcgi.service || exit 1
+	systemctl daemon-reload || exit 1
+	systemctl enable --now floppcgi || exit 1
+fi
+
